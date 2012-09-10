cb57450da9ef54824179acc2d92bc8316c44aaee
Caesar Chi <caesar_chi@miiicasa.com>
2012-09-11 01:05:22 +0800

    updat app.js and index.ejs, update them to main view.

--- app.js
+++ app.js
@@ -19,11 +19,13 @@ app.configure(function(){
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
+  app.use(express.cookieParser('your secret here'));
   app.use(express.session());
   app.use(app.router);
   app.use(require('stylus').middleware(__dirname + '/public'));
   app.use(express.static(path.join(__dirname, 'public')));
 });
+
 
 app.configure('development', function(){
   app.use(express.errorHandler());
