2495ab4a43f02d67c5dd1554946f69260e268a0d
Caesar Chi <clonncd@gmail.com>
2012-09-18 06:26:52 +0800

    update io.client.js, add form submit event

--- public/javascripts/io.client.js
+++ public/javascripts/io.client.js
@@ -6,7 +6,21 @@
  */
 (function ($) {
   $(document).ready(function () {
-  var socket = io.connect();
+
+    var socket = io.connect(),
+        formNode = $('form'),
+        num1Node = formNode.find('input[name=num1]'), 
+        num2Node = formNode.find('input[name=num2]'), 
+        num2Node = formNode.find('input[name=ans]');
+
+
+    // form submit event
+    formNode.on('submit', function (e) {
+
+      e.preventDefault();
+
+    
+    });
 
   });
 
